9330d26ac2d6db1130f7a5cf635ba1005ad079e7
Valentina Zadrija <valentina.zadrija@gmail.com>
2025-10-05 23:10:58 +0200

    WIP act clip

--- examples/2_evaluate_pretrained_policy_rbyte.py
+++ examples/2_evaluate_pretrained_policy_rbyte.py
@@ -26,7 +26,7 @@ def _eval_yaak(hydra_cfg: DictConfig) -> None:
 
     output_dir = (
         Path("/nasa/team-space/artifacts/predictions/lerobot/")
-        / Path(hydra_cfg.model.artifact).name / "same_noise_holdout_clip"
+        / Path(hydra_cfg.model.artifact).name / "same_noise_act_clip"
     )
     output_dir.mkdir(parents=True, exist_ok=True)
     samples.write_parquet(output_dir / "dataset.parquet.bak")  # noqa: ERA001
